e5e293ce63dd58fa2ec15c93cfaa034db9457395
Marco Perasso <marco.perasso@gmail.com>
2014-11-10 10:29:31 +0100

    corrected error pages

--- application/errors/error_general.php
+++ application/errors/error_general.php
@@ -1,24 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php include 'application/views/templates/header.php'; ?>
-        <title>Errore</title>
-        <style>p{text-align: center;}</style>
+        <link rel="import" href="templates/header.html">
+        <title>General error</title>
     </head>
 
     <body>
-        <div class="content">
-
-            <div class="body center-text row">
+        <div class="content" >
+              <div class="body center-text row">
                 <div class="col-md-2"></div>
                 <div class="col-md-8">
-                    <h1><?php echo $heading; ?></h1>
-                    <img class="centered" src="application/asset/img/crashedfrog.png"/>
-                    <?php echo $message; ?>
+                   Sorry, an error has occurred.
                 </div>
                 <div class="col-md-2"></div>
             </div>
-            <?php include 'application/views/templates/footer.php'; ?>
         </div>
     </body>
 </html>
